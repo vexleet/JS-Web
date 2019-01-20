3594135d889abf02c318d83258d3d82d6b10d412
vexleet <vex_malamov@abv.bg>
2019-01-20 16:58:49 +0200

    Completed exercise

--- ExpressJS-Fundamentals/ExerciseNodeMovieDB/handlers/addMovie.js
+++ ExpressJS-Fundamentals/ExerciseNodeMovieDB/handlers/addMovie.js
@@ -1,6 +1,6 @@
 const url = require('url');
 const fs = require('fs');
-var qs = require('querystring');
+const qs = require('querystring');
 let database = require('../config/database');
 
 module.exports = (req, res) => {
@@ -26,8 +26,7 @@ module.exports = (req, res) => {
             res.write(data);
             res.end();
         });
-    }
-    else if(req.pathname === '/addMovie' && req.method === 'POST'){
+    } else if (req.pathname === '/addMovie' && req.method === 'POST') {
         let body = '';
 
         req.on('data', function (data) {
@@ -36,23 +35,45 @@ module.exports = (req, res) => {
 
         req.on('end', function () {
             let post = qs.parse(body);
+            let content = '';
 
-            // if(movieTitle === '' || moviePoster === ''){
-            //
-            // }
+            if (post.movieTitle === '' || post.moviePoster === '') {
+                content = "<div id=\"errBox\">" +
+                    "<h2 id=\"errMsg\">Please fill all fields</h2> " +
+                    "</div>";
+
+                fs.readFile('./views/addMovie.html', (err, data) => {
+                    let html = data.toString().replace('<div id="replaceMe">{{replaceMe}}</div>', content);
+
+                    res.writeHead(400, {
+                        'Content-Type': 'text/html'
+                    });
+
+                    res.write(html);
+                    res.end();
+                });
+                return;
+            }
             post.id = database.length + 1;
             database.push(post);
-            // TODO: Save movies
 
-            res.writeHead(302, {
-                Location: '/'
+            fs.readFile('./views/addMovie.html', (err, data) => {
+                content = "<div id=\"succssesBox\">" +
+                    "<h2 id=\"succssesMsg\">Movie Added</h2> " +
+                    "</div>";
+
+                let html = data.toString().replace('<div id="replaceMe">{{replaceMe}}</div>', content);
+
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+
+                res.write(html);
+                res.end();
             });
-
-            res.end();
         });
 
-    }
-    else{
+    } else {
         return true;
     }
 };
--- ExpressJS-Fundamentals/ExerciseNodeMovieDB/handlers/movies.js
+++ ExpressJS-Fundamentals/ExerciseNodeMovieDB/handlers/movies.js
@@ -28,7 +28,9 @@ module.exports = (req, res) => {
             for (let product of products) {
                 content += `
                     <div class="movie">
-                        <img class="moviePoster" src="${product.moviePoster}" />                    
+                        <a href="/movies/details/${product.id}">    
+                            <img class="moviePoster" src="${product.moviePoster}" />
+                        </a>                                     
                     </div>`;
             }
 
@@ -37,6 +39,44 @@ module.exports = (req, res) => {
             res.write(html);
             res.end();
         })
+    } else if (req.pathname.startsWith('/movies/details/') === true && req.method === 'GET') {
+        let movieId = Number(req.pathname.split('/')[3]);
+        let movie = database.filter(x => x.id === movieId);
+
+        fs.readFile('./views/details.html', (err, data) => {
+            if (err) {
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+
+                res.write('404 Not Found');
+                res.end();
+                return;
+            } else if (movie.length === 0) {
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+
+                res.write('404 Movie does not exist!');
+                res.end();
+                return;
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+
+            let content = `
+                <div class="content">
+                    <img src="${movie[0].moviePoster}" alt="" />
+                    <h3>Title: ${movie[0].movieTitle}</h3>
+                    <h3>Year: ${movie[0].movieYear}</h3>
+                    <p>${movie[0].movieDescription}</p>
+                </div>`;
+            let html = data.toString().replace('<div id="replaceMe">{{replaceMe}}</div>', content);
+            res.write(html);
+            res.end();
+        });
     } else {
         return true;
     }
