011c507bae974683ce7ca3a1235d0d24d1ffbb98
vexleet <vex_malamov@abv.bg>
2019-02-11 16:53:32 +0200

    Board is being drawn by two loops insead of hardcoding

--- ReactJS-Fundamentals/IntroToReact/src/index.js
+++ ReactJS-Fundamentals/IntroToReact/src/index.js
@@ -21,24 +21,26 @@ class Board extends React.Component {
         );
     }
 
+    drawBoard() {
+        let div = [];
+        let squareNumber = 0;
+
+        for(let i = 0; i < 3; i++){
+            let insideDiv = [];
+            for(let j = 0; j < 3; j++){
+                insideDiv.push(this.renderSquare(squareNumber));
+                squareNumber++;
+            }
+            div.push(<div className="board-row">{insideDiv}</div>)
+        }
+
+        return div;
+    }
+
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {this.drawBoard()}
             </div>
         );
     }
